775ed06eddb0babe642084822af5e4d8d4551847
Vjeran Cvitanić <92399175+VjeranCvitanic@users.noreply.github.com>
2022-09-15 21:42:04 +0200

    Delete randomTetronimo.py

--- randomTetronimo.py
+++
@@ -1,5 +0,0 @@
-from tetrominoes import *
-from random import choice
-
-def randomTetronimo():
-    return choice([StraightTetronimo(), SquareTetronimo(), LTetronimo(), JTetronimo(), TTetronimo(), ZTetronimo(), STetronimo()])
