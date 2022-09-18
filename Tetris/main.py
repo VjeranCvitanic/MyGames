440c3aa0e6b6264fdb4a48ed37e6b95c612a0344
Vjeran Cvitanić <92399175+VjeranCvitanic@users.noreply.github.com>
2022-09-18 16:03:19 +0200

    Update main.py

--- Tetris/main.py
+++ Tetris/main.py
@@ -3,6 +3,7 @@ from grid import Grid
 from randomTetronimo import randomTetronimo
 import os
 from copy import deepcopy
+from random import choice
 os.environ["SDL_VIDEODRIVER"] = "dummy"
 
 
@@ -23,8 +24,9 @@ def main():
     key_get_pressed_timer = 1
     fall_timer = 400 #330
     speed_up_the_game_timer = 1
-    speed_up_the_game_mod = 18000
-    speed_up_factor = 0.97
+    speed_up_the_game_mod = 55000
+    speed_up_factor = 5
+    sensitivity = 115
 
     with open('highscore.txt') as f:
         highscore = f.readlines()
@@ -52,9 +54,20 @@ def main():
                     new_tetronimo = return_value
                     new_tetronimo.positions_list = deepcopy(new_tetronimo.startPositions_list)
                     new_grid.tetronimoes_list.append(new_tetronimo)
+            elif event.type == pygame.KEYUP and event.key == pygame.K_p:
+                pause_flag = True
+                while pause_flag:
+                    pause_img = pygame.image.load("pause_img.jpg").convert_alpha()
+                    picture = pygame.transform.scale(pause_img, (80, 80))
+                    screen.blit(picture, (width / 2 - 40, height / 2 - 40))
+
+                    for event in pygame.event.get():
+                        if event.type == pygame.KEYUP:
+                            pause_flag = False
+                    pygame.display.flip()
 
 
-        if key_get_pressed_timer % 80 == 0:
+        if key_get_pressed_timer % sensitivity == 0:
             screen.fill('black')
             new_grid.draw(screen)
             key = pygame.key.get_pressed()
@@ -64,8 +77,16 @@ def main():
 
         if key_get_pressed_timer % fall_timer == 0:
             if speed_up_the_game_timer % speed_up_the_game_mod == 0:
-                fall_timer = int(fall_timer * speed_up_factor)
+                fall_timer = fall_timer - speed_up_factor
+                if fall_timer < 250:
+                    speed_up_factor = 3
+                elif fall_timer < 100:
+                    speed_up_factor = 0
                 speed_up_the_game_timer = 1
+                sensitivity += 5
+                new_grid.level += 1
+                new_grid.lvl_color = choice(['purple', 'blue', 'light blue', 'yellow', 'orange', 'red', 'pink', 'light green'])
+
             speed_up_the_game_timer += 1
             screen.fill('black')
             new_grid.draw(screen)
